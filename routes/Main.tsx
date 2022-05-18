cc4e59c645f2ce05ef416cc1b9e85d787944f459
CHA BONGSEOK <98582315+charBS0701@users.noreply.github.com>
2022-05-18 19:50:43 +0900

    네비게이션 바 아이콘 변경

--- routes/Main.tsx
+++ routes/Main.tsx
@@ -2,24 +2,44 @@
  * @format
  * @flow strict-local
  */
- 
- import React from 'react';
- import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
- import Ranking from './Ranking';
- import MyPage from './MyPage';
- import ClubList from './ClubList';
- 
- const Tab = createBottomTabNavigator();
- 
- const Main = () => {
-   return (
-       <Tab.Navigator>
-         <Tab.Screen name="ClubList" component={ClubList} />
-         <Tab.Screen name="Ranking" component={Ranking} />
-         <Tab.Screen name="MyPage" component={MyPage} />
-       </Tab.Navigator>
-   );
- };
- 
- export default Main;
- 
+
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ranking from "./Ranking";
+import MyPage from "./MyPage";
+import ClubList from "./ClubList";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+const Main = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="동아리 리스트"
+        component={ClubList}
+        options={{
+          tabBarIcon: () => (
+            <MaterialCommunityIcons
+              name="format-list-bulleted"
+              size={24}
+              color="black"
+            />
+          ),
+        }}
+      />
+      <Tab.Screen name="동아리 랭킹" component={Ranking} options={{
+        tabBarIcon: () => (
+          <MaterialCommunityIcons name="chess-king" size={24} color="black" />
+        ),
+      }}/>
+      <Tab.Screen name="마이페이지" component={MyPage} options={{
+        tabBarIcon: () => (
+          <MaterialCommunityIcons name="account" size={24} color="black" />
+        ),
+      }}/>
+    </Tab.Navigator>
+  );
+};
+
+export default Main;

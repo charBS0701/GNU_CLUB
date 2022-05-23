267854220b2ff47e3e9cabd7a73ebb6b4f143e9f
as1248 <jun76248@naver.com>
2022-05-23 18:01:03 +0900

    Posting 수정

--- routes/Posting.tsx
+++ routes/Posting.tsx
@@ -5,7 +5,8 @@ import styled from 'styled-components/native';
 const Title = styled.View`
     display: flex;
     border: 1px solid black;
-    margin: 5% 10%;
+    margin: 0 10%;
+    margin-top: 15%;
     padding-left: 5%;
 `;
 
@@ -13,7 +14,7 @@ const Detail = styled.View`
     height: 30%;
     display: flex;
     border: 1px solid black;
-    margin: 5% 10%;
+    margin: 10%;
     padding-left: 5%;
 `;
 
@@ -21,10 +22,10 @@ const Posting = ({navigation}) => {
     return (
         <View>
             <Title>
-                <TextInput placeholder='title' />
+                <TextInput placeholder='title' maxLength={100}/>
             </Title>
             <Detail>
-                <TextInput placeholder='detail' />
+                <TextInput placeholder='detail' maxLength={5000}/>
             </Detail>
                 <Button title='사진 불러오기'/>
             <Button title='Post' onPress={() => navigation.goBack()} />

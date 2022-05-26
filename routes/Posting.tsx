2081ae945f4e8c38d56c0617568f3de2b5b542f7
as1248 <jun76248@naver.com>
2022-05-26 21:19:23 +0900

    Posting(아직 기능X)

--- routes/Posting.tsx
+++ routes/Posting.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import {View,Text, TextInput, Button} from 'react-native';
+import React, { useState } from 'react';
+import {View, TextInput, Button} from 'react-native';
 import styled from 'styled-components/native';
+import axios from "axios";
 
 const Title = styled.View`
     display: flex;
@@ -18,17 +19,50 @@ const Detail = styled.View`
     padding-left: 5%;
 `;
 
-const Posting = ({navigation}) => {
+
+
+const Posting = ({navigation}, clubPk:number) => {
+    let title:String;
+    let content:String;
+    const callApi = async() => {
+        try{
+            await axios.post(`http://15.165.169.129/api/club/${clubPk}/bulletin_board/notice`,{
+                notice:{
+                    title: title,
+                    content: content,
+                },
+                image:{
+                    src:'',
+                }
+        });}catch(error){
+            console.log(error);
+        }
+    }
+    const post = () => {
+        callApi();
+        alert("게시물이 게시되었습니다.");
+        navigation.goBack();
+    }
     return (
         <View>
             <Title>
-                <TextInput placeholder='title' maxLength={100}/>
+                <TextInput 
+                placeholder='title' 
+                maxLength={100} 
+                onChangeText={(event) => {
+                    title = event;
+                }}/>
             </Title>
             <Detail>
-                <TextInput placeholder='detail' maxLength={5000}/>
+                <TextInput 
+                placeholder='detail' 
+                maxLength={5000}
+                onChangeText={(event) => {
+                    content = event;
+                }}/>
             </Detail>
-                <Button title='사진 불러오기'/>
-            <Button title='Post' onPress={() => navigation.goBack()} />
+                <Button title='사진 불러오기' onPress={() => 1}/>
+            <Button title='게시' onPress={() => post()} />
         </View>
     );
 };

db58a3fd69afce1de4657accad96fd5baf46a8ce
as1248 <jun76248@naver.com>
2022-05-27 19:56:45 +0900

    사진 불러오기(1장만 가능) 기능 추가

--- routes/Posting.tsx
+++ routes/Posting.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import {View, TextInput, Button} from 'react-native';
+import {View, TextInput, Button, Image} from 'react-native';
 import styled from 'styled-components/native';
 import axios from "axios";
+import * as ImagePicker from 'expo-image-picker';
 
 const Title = styled.View`
     display: flex;
@@ -19,22 +20,36 @@ const Detail = styled.View`
     padding-left: 5%;
 `;
 
+const Images = styled.View`
+    width: 40%;
+    margin-left: 10%;
+`;
 
 
 const Posting = ({navigation}, clubPk:number) => {
     let title:String;
     let content:String;
+    const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result:any = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1,1],
+      quality: 1,
+    });
+    setImage(result.uri);
+    }
+    
     const callApi = async() => {
         try{
-            await axios.post(`http://15.165.169.129/api/club/${clubPk}/bulletin_board/notice`,{
+            const response = await axios.post(`http://15.165.169.129/api/club/${clubPk}/bulletin_board/notice`,{
                 notice:{
                     title: title,
                     content: content,
                 },
-                image:{
-                    src:'',
-                }
-        });}catch(error){
+            });
+            }catch(error){
             console.log(error);
         }
     }
@@ -43,6 +58,8 @@ const Posting = ({navigation}, clubPk:number) => {
         alert("게시물이 게시되었습니다.");
         navigation.goBack();
     }
+
+
     return (
         <View>
             <Title>
@@ -61,7 +78,10 @@ const Posting = ({navigation}, clubPk:number) => {
                     content = event;
                 }}/>
             </Detail>
-                <Button title='사진 불러오기' onPress={() => 1}/>
+            <Images>
+                <Image source={{ uri: image }} style={{ width: 100, height: 100 }} />
+                <Button title='사진 불러오기' onPress={() => pickImage()}/> 
+            </Images>
             <Button title='게시' onPress={() => post()} />
         </View>
     );

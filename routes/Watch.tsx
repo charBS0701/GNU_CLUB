b9bd08d480275751b58557aff39f54b9b2c9355b
as1248 <jun76248@naver.com>
2022-05-27 14:14:19 +0900

    Watch 기능 추가

--- routes/Watch.tsx
+++ routes/Watch.tsx
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import {View,Text,Image, Button, TextInput} from 'react-native';
+import {View,Text,Image, Button, TextInput, ActivityIndicator} from 'react-native';
 import styled from 'styled-components/native';
 import axios from "axios";
 import { AntDesign } from '@expo/vector-icons';
 import { EvilIcons } from '@expo/vector-icons';
 
 const Title = styled.Text`
-    margin: 10%;
+    margin-top: 15%;
+    margin-left: 8%;
     font-size: 30px;
 `;
 
 const Time = styled.Text`
-    margin-left: 5%;
+    margin-left: 10%;
 `;
 
 const Detail = styled.Text`
-    margin: 5%;
+    margin: 10%;
 `;
 
 const Counting = styled.View`
     display: flex;
     flex-direction: row;
-    padding: 5%;
+    padding: 8%;
 `;
 
 const AddComment = styled.View`
@@ -47,59 +48,68 @@ const Like = styled.View`
     
 `;
 
-const Watch = (noticePk) => {
+const Watch = (noticePk:any) => {
     const [like,setLike] = useState(false);
     const [notice,setNotice] = useState<any>();
+    const [loading,setLoading] = useState(true);
     const callApi = async() => {
         try{
             const response = await axios.get(`http://15.165.169.129/api/club/notice/${noticePk.route.params.noticePk}?member_pk=1`);
             setNotice(response.data.data);
-            setTimeout(()=>'',1000);
-            console.log(notice);
+            setLoading(false);
             }catch(error){
                 console.log(error);
             };
     }
+    
+    console.log(notice);
     useEffect(() => {callApi()},[]);
     return (
         <View>
-            <Title>{notice.title}</Title>
-            <Time>{notice.postingTime}</Time>
-            <Detail>{notice.content}</Detail>
-            <Image source={{uri:`${notice.imageUrl}`}}/>
-            <Counting>
-                <Like onPress={() => setLike(true)}>
-                    {(like) ? (
-                    <AntDesign name="heart" size={24} color="black" />
-                    ) : (
-                    <AntDesign name="hearto" size={24} color="black" />
-                    )}
-                </Like>
-                <Text> {notice.likeCount} </Text>
-                <EvilIcons name="comment" size={24} color="black" />
-                <Text> comment count </Text>
-            </Counting>
-
-            <AddComment>
-                <CommentInput>
-                    <TextInput maxLength={300}/>
-                </CommentInput>
-                <Button title='게시'/>
-            </AddComment>
-
-            <CommentList>
-                <Comment>
-                    <Text>id</Text>
-                    <Text>detail</Text>
-                </Comment>
-                <Comment>
-                    <Text>댓글2</Text>
-                </Comment>
-                <Comment>
-                    <Text>댓글3</Text>
-                </Comment>
-            </CommentList>
+            {loading ? (<View>
+                <ActivityIndicator size="large" />
+            </View>) : (
+                <View>
+                <Title>{notice.title}</Title>
+                <Time>{notice.postingTime}</Time>
+                <Detail>{notice.content}</Detail>
+                <Image source={{uri:`${notice.imageUrl}`}}/>
+                <Counting>
+                    <Like onPress={() => setLike(true)}>
+                        {(like) ? (
+                        <AntDesign name="heart" size={24} color="black" />
+                        ) : (
+                        <AntDesign name="hearto" size={24} color="black" />
+                        )}
+                    </Like>
+                    <Text> {notice.likeCount} </Text>
+                    <EvilIcons name="comment" size={24} color="black" />
+                    <Text> comment count </Text>
+                </Counting>
+        
+                <AddComment>
+                    <CommentInput>
+                        <TextInput maxLength={300}/>
+                    </CommentInput>
+                    <Button title='게시'/>
+                </AddComment>
+        
+                <CommentList>
+                    <Comment>
+                        <Text>id</Text>
+                        <Text>detail</Text>
+                    </Comment>
+                    <Comment>
+                        <Text>댓글2</Text>
+                    </Comment>
+                    <Comment>
+                        <Text>댓글3</Text>
+                    </Comment>
+                </CommentList> 
+                </View>
+            ) }    
         </View>
+        
     );
 };
 
